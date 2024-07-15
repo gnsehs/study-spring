cec229607e183768a681a213af8a8371919e1399
DoHun Han <gks9315@naver.com>
2024-07-15 18:54:41 +0900

    feat: mvc 1편 - 섹션 3. 서블릿, JSP, MVC 패턴 (회원 관리 웹 애플리케이션 요구사항)

---
+++ mvc-1/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,40 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    private MemberRepository() {
+
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); // store에 있는 모든 값을 꺼내서 ArrayList로 넘겨서 줌
+        // store 자체 보호
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
