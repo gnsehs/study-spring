e430e3d754d3c9a4b44c4ad97a6e450a47d8daa0
gnsehs <gks9315@naver.com>
2024-07-22 17:47:01 +0900

    build: add 스프링 MVC - 기본 기능 project

---
+++ mvc-1-springmvc/src/main/java/hello/mvc1springmvc/Mvc1SpringmvcApplication.java
@@ -0,0 +1,13 @@
+package hello.mvc1springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc1SpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mvc1SpringmvcApplication.class, args);
+    }
+
+}
---
+++ mvc-1-springmvc/src/test/java/hello/mvc1springmvc/Mvc1SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.mvc1springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc1SpringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
