23b021df016fa75b9e8a7d17a3bb3a3901c365aa
DoHun Han <gks9315@naver.com>
2024-07-25 22:52:05 +0900

    build: add 스프링 MVC - 웹 페이지 만들기 project

---
+++ mvc-1-itemservice/src/main/java/hello/mvc1itemservice/Mvc1ItemserviceApplication.java
@@ -0,0 +1,13 @@
+package hello.mvc1itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc1ItemserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mvc1ItemserviceApplication.class, args);
+    }
+
+}
