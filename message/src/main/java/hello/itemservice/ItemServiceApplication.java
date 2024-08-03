52f9256f02443847d4296d2bafc7e229a05f2008
DoHun Han <gks9315@naver.com>
2024-08-03 22:38:55 +0900

    feat: mvc 2편 - 섹션3. 메시지, 국제화

--- message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -3,10 +3,13 @@ package hello.itemservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.KOREA); // for wsl
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
---
+++ message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,69 @@
+package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    MessageSource ms;
+
+    /**
+     * wsl에서 default locale을 한글로 바꾸기 위함
+     */
+    @BeforeAll
+    static void setDefaultLocale() {
+        Locale.setDefault(Locale.KOREA);
+    }
+
+    @Test
+    void helloMessage() {
+        String result = ms.getMessage("hello", null, null);
+        assertThat(result).isEqualTo("안녕");
+    }
+
+    /**
+     * NoSuchMessageException
+     */
+    @Test
+    void noFoundMessageCode() {
+        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+
+    @Test
+    void noFoundMessageCodeDefaultMessage() {
+        String defaultMsg = "기본 메세지";
+        String message = ms.getMessage("no_code", null, defaultMsg, null);
+        assertThat(message).isEqualTo(defaultMsg);
+    }
+
+    @Test
+    void argumentMessage() {
+        String message = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+
+        assertThat(message).isEqualTo("안녕 Spring");
+    }
+
+    @Test
+    void defaultLang() {
+        assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
+        assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+    }
+
+    @Test
+    void enLang() {
+        assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+    }
+}
