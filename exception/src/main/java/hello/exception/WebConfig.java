4cb080ce5ea3da44da004f39bc74ad1c136e6b93
DoHun Han <gks9315@naver.com>
2024-08-14 22:30:16 +0900

    feat: mvc 2편 - 섹션 8. 예외 처리와 오류 페이지

---
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,39 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterCeptor;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+//    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR); // 클라이언트 요청 + 오류 페이지 요청 모두 필터 호출
+        // -> 기본값은 DispatcherType.REQUEST 만
+
+        return filterRegistrationBean;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterCeptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico"
+                        , "/error", "/error-page/**"); // 오류 페이지 경로는 인터셉터가 호출되지 않음
+
+    }
+}
---
+++ exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -0,0 +1,34 @@
+package hello.exception.servlet;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Controller
+public class ServletExController {
+
+    /**
+     * exception의 경우 status 500
+     */
+    @GetMapping("/error-ex")
+    public void errorEx() {
+        throw new RuntimeException("예외 발생!");
+    }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_NOT_FOUND, "404 오류!");
+    }
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, "400 오류!");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+
+}
